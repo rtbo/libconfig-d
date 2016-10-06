fe6d47673f0143ed6e7b51dfaece5138703c95b7
Remi THEBAULT <remi.thebault@gmail.com>
2016-10-06 08:10:43 +0200

    attempt to check parsing errors
    cannot get errors out of Pegged at the moment

--- src/config/config.d
+++ src/config/config.d
@@ -145,136 +145,159 @@ class Config
         {
             import cg = config.grammar;
 
-            private {
-
-                static Config readConfig(in string config)
-                {
-                    auto conf = new Config;
-                    auto mainTree = cg.Config(config);
-                    assert(mainTree.children.length == 1);
-                    assert(mainTree.children[0].name == "Config.Document");
-                    auto docTree = mainTree.children[0];
-
-                    foreach (pt; docTree.children) {
-                        assert(pt.name == "Config.Setting");
-                        conf.root.addChild(parseSetting(conf, conf.root, pt));
-                    }
-
-                    return conf;
+            static void checkPT(cg.ParseTree pt)
+            {
+                if (!pt.successful) {
+                    throw new Exception("libconfig-d: error while parsing: "~pt.failMsg());
                 }
+            }
 
-                static Setting parseSetting(Config conf, AggregateSetting par, cg.ParseTree pt) {
+            static Config readConfig(in string config)
+            {
+                auto conf = new Config;
+                auto mainTree = cg.Config(config);
+                checkPT(mainTree);
+
+                assert(mainTree.children.length == 1);
+                assert(mainTree.children[0].name == "Config.Document");
+                auto docTree = mainTree.children[0];
+                checkPT(docTree);
+
+                foreach (pt; docTree.children) {
                     assert(pt.name == "Config.Setting");
-                    // assertions enforced by grammar
-                    assert(pt.children.length == 2);
-                    assert(pt.children[0].name == "Config.Name");
-                    assert(pt.children[1].name == "Config.Value");
-
-                    string name = pt.children[0].matches[0];
-                    return parseValue(conf, par, name, pt.children[1]);
+                    conf.root.addChild(parseSetting(conf, conf.root, pt));
                 }
 
+                return conf;
+            }
 
-                static Setting parseValue(Config conf, AggregateSetting par, string name, cg.ParseTree valTree) {
-                    assert(valTree.name == "Config.Value");
+            static Setting parseSetting(Config conf, AggregateSetting par, cg.ParseTree pt)
+            {
+                assert(pt.name == "Config.Setting");
+                checkPT(pt);
 
-                    switch (valTree.children[0].name) {
-                        case "Config.Scalar":
-                            return parseScalar(conf, par, name, valTree.children[0]);
-                        case "Config.Array": {
-                            auto s = new ArraySetting(conf, par, name);
-                            auto ss = new Setting[valTree.children.length];
-                            foreach (i, pt; valTree.children) {
-                                ss[i] = parseValue(conf, s, "", pt);
-                            }
-                            s.setChildren(ss);
-                            return s;
+                // assertions enforced by grammar
+                assert(pt.children.length == 2);
+                assert(pt.children[0].name == "Config.Name");
+                assert(pt.children[1].name == "Config.Value");
+
+                checkPT(pt.children[0]);
+                string name = pt.children[0].matches[0];
+                return parseValue(conf, par, name, pt.children[1]);
+            }
+
+
+            static Setting parseValue(Config conf, AggregateSetting par, string name, cg.ParseTree valTree)
+            {
+                assert(valTree.name == "Config.Value");
+                checkPT(valTree);
+                valTree = valTree.children[0];
+                switch (valTree.name) {
+                    case "Config.Scalar":
+                        return parseScalar(conf, par, name, valTree);
+                    case "Config.Array": {
+                        auto s = new ArraySetting(conf, par, name);
+                        auto ss = new Setting[valTree.children.length];
+                        foreach (i, pt; valTree.children) {
+                            ss[i] = parseValue(conf, s, "", pt);
                         }
-                        case "Config.List": {
-                            auto s = new ListSetting(conf, par, name);
-                            auto ss = new Setting[valTree.children.length];
-                            foreach (i, pt; valTree.children) {
-                                ss[i] = parseValue(conf, s, "", pt);
-                            }
-                            s.setChildren(ss);
-                            return s;
+                        s.setChildren(ss);
+                        return s;
+                    }
+                    case "Config.List": {
+                        auto s = new ListSetting(conf, par, name);
+                        auto ss = new Setting[valTree.children.length];
+                        foreach (i, pt; valTree.children) {
+                            ss[i] = parseValue(conf, s, "", pt);
                         }
-                        case "Config.Group":
+                        s.setChildren(ss);
+                        return s;
+                    }
+                    case "Config.Group": {
+                        auto s = new GroupSetting(conf, par, name);
+                        auto ss = new Setting[valTree.children.length];
+                        foreach (i, pt; valTree.children) {
+                            ss[i] = parseSetting(conf, s, pt);
+                        }
+                        s.setChildren(ss);
+                        return s;
+                    }
+                    default: assert(false);
+                }
+            }
+
+            static Setting parseScalar(Config conf, AggregateSetting par, string name, cg.ParseTree scalTree)
+            {
+                assert(scalTree.name == "Config.Scalar");
+                checkPT(scalTree);
+
+                switch (scalTree.children[0].name) {
+                    case "Config.Bool": {
+                        import std.uni : toLower;
+                        ScalarSetting s = new ScalarSetting(conf, par, name, Type.Bool);
+                        if (scalTree.matches[0][0].toLower == 't') {
+                            s.value = true;
+                        }
+                        else {
+                            s.value = false;
+                        }
+                        return s;
+                    }
+                    case "Config.Integer": {
+                        import std.conv : to;
+                        ScalarSetting s = new ScalarSetting(conf, par, name, Type.Int);
+                        if (scalTree.children[0].children[0].name == "Config.Dec") {
+                            s.value = scalTree.matches[0].to!long(10);
+                        }
+                        else {
+                            assert(scalTree.children[0].children[0].name == "Config.Hex");
+                            s.value = scalTree.matches[0].to!long(16);
+                        }
+                        return s;
+                    }
+                    case "Config.Float": {
+                        import std.conv : to;
+                        ScalarSetting s = new ScalarSetting(conf, par, name, Type.Float);
+                        s.value = scalTree.matches[0].to!double();
+                        return s;
+                    }
+                    case "Config.String": {
+                        ScalarSetting s = new ScalarSetting(conf, par, name, Type.String);
+                        s.value = scalTree.matches[0];
+                        return s;
+                    }
+                    default: assert(false);
+                }
+            }
+
+            static void validateArrayChildren(Setting[] children) {
+                import std.algorithm : each;
+                import std.exception : enforce;
+                enum seenBool   = 1;
+                enum seenInt    = 2;
+                enum seenFloat  = 4;
+                enum seenString = 8;
+                static int seen (in Type t) {
+                    switch(t) {
+                        case Type.Bool: return seenBool;
+                        case Type.Int: return seenInt;
+                        case Type.Float: return seenFloat;
+                        case Type.String: return seenString;
                         default: assert(false);
                     }
                 }
 
-                static Setting parseScalar(Config conf, AggregateSetting par, string name, cg.ParseTree scalTree) {
-                    assert(scalTree.name == "Config.Scalar");
+                int seenFl;
+                children.each!(c => seenFl |= seen(c.type));
 
-                    switch (scalTree.children[0].name) {
-                        case "Config.Bool": {
-                            import std.uni : toLower;
-                            ScalarSetting s = new ScalarSetting(conf, par, name, Type.Bool);
-                            if (scalTree.matches[0][0].toLower == 't') {
-                                s.value = true;
-                            }
-                            else {
-                                s.value = false;
-                            }
-                            return s;
-                        }
-                        case "Config.Integer": {
-                            import std.conv : to;
-                            ScalarSetting s = new ScalarSetting(conf, par, name, Type.Int);
-                            if (scalTree.children[0].children[0].name == "Config.Dec") {
-                                s.value = scalTree.matches[0].to!long(10);
-                            }
-                            else {
-                                assert(scalTree.children[0].children[0].name == "Config.Hex");
-                                s.value = scalTree.matches[0].to!long(16);
-                            }
-                            return s;
-                        }
-                        case "Config.Float": {
-                            import std.conv : to;
-                            ScalarSetting s = new ScalarSetting(conf, par, name, Type.Float);
-                            s.value = scalTree.matches[0].to!double();
-                            return s;
-                        }
-                        case "Config.String": {
-                            ScalarSetting s = new ScalarSetting(conf, par, name, Type.String);
-                            s.value = scalTree.matches[0];
-                            return s;
-                        }
-                        default: assert(false);
-                    }
-                }
-
-                static void validateArrayChildren(Setting[] children) {
-                    import std.algorithm : each;
-                    import std.exception : enforce;
-                    enum seenBool   = 1;
-                    enum seenInt    = 2;
-                    enum seenFloat  = 4;
-                    enum seenString = 8;
-                    static int seen (in Type t) {
-                        switch(t) {
-                            case Type.Bool: return seenBool;
-                            case Type.Int: return seenInt;
-                            case Type.Float: return seenFloat;
-                            case Type.String: return seenString;
-                            default: assert(false);
-                        }
-                    }
-
-                    int seenFl;
-                    children.each!(c => seenFl |= seen(c.type));
-
-                    enforce(seenFl == seenBool || seenFl == seenString || seenFl & (seenInt | seenFloat));
-                    if ((seenFl & seenInt)==seenInt && (seenFl & seenFloat)==seenFloat) {
-                        // mix of float and ints, convert all ints to float
-                        foreach(ref c; children) {
-                            if (c.type == Type.Int) {
-                                auto sc = cast(ScalarSetting)c;
-                                immutable val = sc.value!long;
-                                sc.value = cast(double)val;
-                            }
+                enforce(seenFl == seenBool || seenFl == seenString || seenFl & (seenInt | seenFloat));
+                if ((seenFl & seenInt)==seenInt && (seenFl & seenFloat)==seenFloat) {
+                    // mix of float and ints, convert all ints to float
+                    foreach(ref c; children) {
+                        if (c.type == Type.Int) {
+                            auto sc = cast(ScalarSetting)c;
+                            immutable val = sc.value!long;
+                            sc.value = cast(double)val;
                         }
                     }
                 }
